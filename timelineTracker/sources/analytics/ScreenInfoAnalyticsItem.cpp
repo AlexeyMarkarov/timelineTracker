7fd4f0a5a1570ef897e83a680b03d6043f04cad0
Alexey Markarov <olexiy.markarov@gmail.com>
2017-10-16 14:42:49 +0300

    Added screen color depth analytics

--- timelineTracker/sources/analytics/ScreenInfoAnalyticsItem.cpp
+++ timelineTracker/sources/analytics/ScreenInfoAnalyticsItem.cpp
@@ -12,7 +12,7 @@ QVector<QPair<QString, QString>> ScreenInfoAnalyticsItem::createPayload() const
 {
     QVector<QPair<QString, QString>> payload;
     const QSize screenSize = qApp->primaryScreen()->size();
-    payload.append(qMakePair(QStringLiteral("sr"),
-                             QStringLiteral("%1x%2").arg(screenSize.width()).arg(screenSize.height())));
+    payload.append(qMakePair(QStringLiteral("sr"), QStringLiteral("%1x%2").arg(screenSize.width()).arg(screenSize.height())));
+    payload.append(qMakePair(QStringLiteral("sd"), QString::number(qApp->primaryScreen()->depth())));
     return payload;
 }
